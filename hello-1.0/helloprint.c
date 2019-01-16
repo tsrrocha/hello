75580789bf64dede5e5708f102da9baef40ce4d3
Tiago Sousa Rocha <tsrrocha@gmail.com>
2019-01-16 11:20:12 -0200

    Update helloprint.c
    ...

--- hello-1.0/helloprint.c
+++ hello-1.0/helloprint.c
@@ -2,5 +2,7 @@
 #include "helloprint.h"
 
 void printHello( void ) {
-    fprintf(stdout, "Hello - DEBUG - Ola, mundo legal!!!");
+    fprintf(stdout, "\n");
+    fprintf(stdout, "Hello - DEBUG - Ola, mundo legal!!!\n");
+    fprintf(stdout, "\n");
 }
